c3b037e34880ba2a0f5ee578b76d88d63f937f94
mer6371 <62220907+mer6371@users.noreply.github.com>
2020-04-13 22:56:10 -0400

    All projects created

---
+++ TutorTrac2.0.WebUI/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TutorTrac2._0.WebUI
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
