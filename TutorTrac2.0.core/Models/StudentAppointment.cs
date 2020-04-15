873dbbb01224170cfc607b2c691af353fd2b9355
mer6371 <62220907+mer6371@users.noreply.github.com>
2020-04-15 10:33:24 -0400

    Student Appointment Model and Repository created

---
+++ TutorTrac2.0.core/Models/StudentAppointment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorTrac2.core.Models
+{
+    public class StudentAppointment
+    {
+        public string Id { get; set; }
+
+        [DisplayName("Tutor")]
+        public int tut_id { get; set; }
+
+        public DateTime dateTime { get; set; }
+
+        [DisplayName("Class")]
+        public string ses_class { get; set; }
+
+        public StudentAppointment()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
