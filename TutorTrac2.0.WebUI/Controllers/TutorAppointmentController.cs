f374e8ea74d9c3d69b75455bbea489cd6e72a62b
mer6371 <62220907+mer6371@users.noreply.github.com>
2020-04-15 11:59:37 -0400

    Everything created for tutor appointments. Need to fix the details link later

---
+++ TutorTrac2.0.WebUI/Controllers/TutorAppointmentController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TutorTrac2.core.Models;
+using TutorTrac2.DataAccess.InMemory;
+
+namespace TutorTrac2.WebUI.Controllers
+{
+    public class TutorAppointmentController : Controller
+    {
+        TutorAppointmentRepository context;
+
+        public TutorAppointmentController()
+        {
+            context = new TutorAppointmentRepository();
+        }
+
+        //Get: TutorAppointmentManager
+        public ActionResult Index()
+        {
+            List<TutorAppointment> tutorAppointments = context.Collection().ToList();
+            return View(tutorAppointments);
+        }
+
+        public ActionResult Create()
+        {
+            TutorAppointment tutorAppointment = new TutorAppointment();
+            return View(tutorAppointment);
+        }
+
+        [HttpPost]
+        public ActionResult Create(TutorAppointment tutorAppointment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tutorAppointment);
+            }
+            else
+            {
+                context.Insert(tutorAppointment);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            TutorAppointment tutorAppointment = context.Find(Id);
+            if (tutorAppointment == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(tutorAppointment);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(TutorAppointment tutorAppointment, string Id)
+        {
+            TutorAppointment tutorAppointmentToEdit = context.Find(Id);
+            if (tutorAppointment == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(tutorAppointment);
+                }
+
+                tutorAppointmentToEdit.stu_fname = tutorAppointment.stu_fname;
+                tutorAppointmentToEdit.stu_lname = tutorAppointment.stu_lname;
+                tutorAppointmentToEdit.dateTime = tutorAppointment.dateTime;
+                tutorAppointmentToEdit.ses_class = tutorAppointment.ses_class;
+
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            TutorAppointment tutorAppointmentToDelete = context.Find(Id);
+            if (tutorAppointmentToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(tutorAppointmentToDelete);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            TutorAppointment tutorAppointmentToDelete = context.Find(Id);
+            if (tutorAppointmentToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.delete(Id);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ TutorTrac2.0.core/Models/TutorAppointment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorTrac2.core.Models
+{
+    public class TutorAppointment
+    {
+        public string Id { get; set; }
+
+        [DisplayName("Student First Name")]
+        public string stu_fname { get; set; }
+
+        [DisplayName("Student Last Name")]
+        public string stu_lname { get; set; }
+
+        [DisplayName("Date & Time")]
+        public DateTime dateTime { get; set; }
+
+        [DisplayName("Class")]
+        public string ses_class { get; set; }
+
+        public TutorAppointment()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
