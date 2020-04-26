1858b727132c60d62ffeedd196ed47b3d38c3282
mer6371 <62220907+mer6371@users.noreply.github.com>
2020-04-25 23:21:14 -0400

    Database Created

---
+++ TutorTrac2.0.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TutorTrac2.core.Models;
+
+namespace TutorTrac2.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            :base("DefaultConnection")
+        {
+        }
+
+        public DbSet<ClassGrouping> Classes { get; set; }
+        public DbSet<ClassTutors> ClassTutors { get; set; }
+        public DbSet<StudentAppointment> StudentAppointments { get; set; }
+        public DbSet<StudentSchedule> StudentSchedules { get; set; }
+        public DbSet<TutorAppointment> TutorAppointments { get; set; }
+        public DbSet<Tutors> Tutors { get; set; }
+        public DbSet<TutorSchedule> TutorSchedules { get; set; }
+    }
+}
---
+++ TutorTrac2.0.DataAccess.SQL/Migrations/202004260301335_Initial.cs
@@ -0,0 +1,108 @@
+﻿namespace TutorTrac2._0.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ClassGroupings",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Class_Code = c.String(nullable: false, maxLength: 128),
+                        Class_Title = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Class_Code);
+            
+            CreateTable(
+                "dbo.ClassTutors",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Class_Code = c.String(),
+                        tut_id = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+                
+            
+            CreateTable(
+                "dbo.StudentAppointments",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        tut_f_name = c.String(),
+                        tut_l_name = c.String(),
+                        dateTime = c.DateTime(nullable: false),
+                        ses_class = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.StudentSchedules",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        c_code = c.String(),
+                        professor = c.String(),
+                        day = c.String(),
+                        time = c.String(),
+                        room = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.TutorAppointments",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        stu_fname = c.String(),
+                        stu_lname = c.String(),
+                        dateTime = c.DateTime(nullable: false),
+                        ses_class = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tutors",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        tut_f_name = c.String(),
+                        tut_l_name = c.String(),
+                        tut_email = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.TutorSchedules",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        day = c.Int(nullable: false),
+                        time = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.TutorSchedules");
+            DropTable("dbo.Tutors");
+            DropTable("dbo.TutorAppointments");
+            DropTable("dbo.StudentSchedules");
+            DropTable("dbo.StudentAppointments");
+            DropTable("dbo.ClassTutors");
+            DropTable("dbo.ClassGroupings");
+        }
+    }
+}
